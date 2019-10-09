8c5005907a43f3edc9f043933d97f8a3c68f3dd3
DakKilvari <54546630+DakKilvari@users.noreply.github.com>
2019-10-09 14:48:18 +0200

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext.commands import Bot
+
+BOT_PREFIX = (".")
+TOKEN = "YOUR TOKEN"
+
+bot = Bot(command_prefix=BOT_PREFIX)
+
+@bot.event
+async def on_ready():
+    game = discord.Game(".help")
+    await bot.change_presence(status=discord.Status.online, activity=game)
+
+extensions = ['cmd_TW', 'cmd_Top80', 'error_handler']
+
+if __name__ == '__main__':
+    for extension in extensions:
+        try:
+            bot.load_extension(extension)
+            print('{} is loaded.'.format(extension))
+        except Exception as error:
+            print('{} cannot be loaded. [{}]'.format(extension, error))
+
+bot.run(TOKEN)
